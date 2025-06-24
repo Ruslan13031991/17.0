acf3893d6bf4acc2f4a6f4a13df990dec94a866f
Ruslan <ruslan.kolomeycev13@gmail.com>
2025-06-24 13:34:51 +0300

    122

--- neuron/__manifest__.py
+++ neuron/__manifest__.py
@@ -11,6 +11,7 @@
     'author': "Neuron",
     'website': "https://neuron-systems.com",
 
+
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/17.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
